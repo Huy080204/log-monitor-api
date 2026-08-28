cde81cdf6134e91316433c048711d31af7d242b5
huypham <huyphuocbinh204@gmail.com>
2026-08-28 10:54:16 +0700

    first commit

---
+++ source/log-monnitor-api/src/main/java/log/monitor/api/service/feign/FeignConst.java
@@ -0,0 +1,6 @@
+package log.monitor.api.service.feign;
+
+public class FeignConst {
+    public static final String LOGIN_TYPE_INTERNAL = "LOGIN_TYPE_INTERNAL";
+    public static final String LOGIN_TYPE_NO_AUTH = "LOGIN_TYPE_NO_AUTH";
+}
