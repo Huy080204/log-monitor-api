69637c88492034777eb50c1ba466318ec5f66d3c
huypham <huyphuocbinh204@gmail.com>
2026-08-28 14:31:30 +0700

    handle query

--- source/log-monnitor-api/src/main/java/log/monitor/api/repository/NotificationQueryRepository.java
+++ source/log-monnitor-api/src/main/java/log/monitor/api/repository/NotificationQueryRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public interface NotificationQueryRepository extends JpaRepository<NotificationQuery, Long>, JpaSpecificationExecutor<NotificationQuery> {
 
     @Query(value = "SELECT COUNT(*) > 0 FROM db_notification_query WHERE BINARY query = :query AND notification_group_id = :notificationGroupId", nativeQuery = true)
     boolean existsByQueryAndNotificationGroupIdCaseSensitive(@Param("query") String query, @Param("notificationGroupId") Long notificationGroupId);
+
+    List<NotificationQuery> findAllByNotificationGroupIdAndStatus(Long notificationGroupId, Integer status);
 
     @Modifying
     @Transactional
