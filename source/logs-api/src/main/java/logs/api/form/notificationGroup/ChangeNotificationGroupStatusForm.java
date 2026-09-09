cbc93b7cad48409b4b96e0ef5a0e6d4eda6a268e
minh <minhminh20925@gmail.com>
2026-09-09 07:59:26 +0700

    update form change state notification group

--- source/logs-api/src/main/java/logs/api/form/notificationGroup/ChangeNotificationGroupStatusForm.java
+++ source/logs-api/src/main/java/logs/api/form/notificationGroup/ChangeNotificationGroupStatusForm.java
@@ -1,12 +1,21 @@
 package logs.api.form.notificationGroup;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
+import logs.api.form.StringToLongDeserializer;
 import logs.api.validation.NotificationGroupStatus;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Schema
 public class ChangeNotificationGroupStatusForm {
+    @NotNull(message = "id cannot be null")
+    @JsonDeserialize(using = StringToLongDeserializer.class)
+    @Schema(name = "id", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Long id;
+
     @NotificationGroupStatus
     @Schema(name = "status", requiredMode = Schema.RequiredMode.REQUIRED)
     private Integer status;
